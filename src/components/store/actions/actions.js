3526ea39e8bf52fc78b1a18801a39137dca808ce
Filip Kaluza <kaluzafilip33@gmail.com>
2021-05-10 10:06:42 +0200

    fix error handling in actions

--- src/components/store/actions/actions.js
+++ src/components/store/actions/actions.js
@@ -30,7 +30,7 @@ export const fetchMovies = (input) => {
             dispatch(fetchMoviesSuccess(movies));
         }
         catch (err) {
-            
+            dispatch(fetchMoviesFailed("Nepodarilo sa načítať filmy"));
         }
     };
 };
